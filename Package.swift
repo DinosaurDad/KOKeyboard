224a559e3321afce26dc03b12053f15fa6ff0b60
DinosaurDad <523433+DinosaurDad@users.noreply.github.com>
2021-11-22 17:24:29 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
             name: "KOKeyboard",
             dependencies: [],
             path: "KOKeyboard",
-            exclude: ["KOKeyboard-Info.plist",
+            exclude: ["en.lproj/",
+                      "KOKeyboard-Info.plist",
                       "KOKeyboard-Prefix.pch",
                       "KOAppDelegate.h",
                       "KOAppDelegate.m",
@@ -21,7 +22,21 @@ let package = Package(
                       "KOViewController.m",
                       "Regular Expression Keys/",
                       "gitignore.txt",
-                      "main.m"]
+                      "main.m"],
+            resources: [
+              .process("key-blue@2x.png"),
+              .process("hal-white.png"),
+              .process("hal-black.png"),
+              .process("key.png"),
+              .process("key-pressed@2x.png"),
+              .process("hal-black@2x.png"),
+              .process("hal-blue@2x.png"),
+              .process("hal-blue.png"),
+              .process("key-blue.png"),
+              .process("key-pressed.png"),
+              .process("hal-white@2x.png"),
+              .process("key@2x.png")
+            ]
         )
     ]
 )
